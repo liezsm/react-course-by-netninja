0272eca1945b04abe6d80c844adeae55dadf440e
liezsm <liez.dev@gmail.com>
2022-03-06 19:36:21 +0800

    Part 2: dynamic values in templates

--- src/reactcourseNetNinja/App.js
+++ src/reactcourseNetNinja/App.js
@@ -5,12 +5,22 @@ import React from 'react'
  const App = () => {
      const title = "Welcome to the new blog";
      const likes = 50;
+     const person = { name: "yoshi", age:30} 
+    //  exp objects and booleans can't be output
+
+    const link = 'http://www.google.con';
+    
 
   return (
     <div>
         <div className="content">
             <h1> { title } </h1>
             <p> Liked { likes } times</p>
+            <p> { 'hello ninjas'}</p>
+            <p> {[1,2,33,4,5]}</p>
+            <p>{Math.random() * 10}</p>
+            <a href={link}>Google</a>
+           
         </div>
     </div>
   )
