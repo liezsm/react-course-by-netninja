06b8d5d83e863ce86170834aa2d91dbd21398b1b
liezsm <liez.dev@gmail.com>
2022-03-07 13:07:32 +0800

    Part: click events

--- src/reactcourseNetNinja/components/Navbar.js
+++ src/reactcourseNetNinja/components/Navbar.js
@@ -1,10 +1,27 @@
 const Navbar = () => {
+  // todo normal func
+  const handleClick = () => {
+    console.log("hello ninjas");
+  };
+  //   todo when passing an argument
+  const handleClickagain = (name) => {
+    console.log(name);
+  };
+
   return (
     <nav className='navbar'>
       <h1>Dojo blog</h1>
       <div className='links'>
         <a href='/'>Home</a>
         <a href='/create'>New blog</a>
+        <button onClick={handleClick}>Click me</button>
+        <button
+          onClick={() => {
+            handleClickagain("test");
+          }}
+        >
+          Click me!
+        </button>
       </div>
     </nav>
   );
